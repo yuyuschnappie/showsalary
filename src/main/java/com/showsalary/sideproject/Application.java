d43dda7d397e4ec0a11c7a4ceaabbf10f8d24ffa
yuyuschnappie <yuyuforjava@gmail.com>
2022-09-16 00:38:12 +0800

    project init

---
+++ src/main/java/com/showsalary/sideproject/Application.java
@@ -0,0 +1,13 @@
+package com.showsalary.sideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
